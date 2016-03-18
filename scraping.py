bc1bf8f5b4bfd4914aa61be0e7690bbad3aff948
Christoph Safferling <christoph.safferling@bluebyte.de>
2016-03-18 14:35:41 +0100

    use pandas and requests, python3

--- scraping.py
+++ scraping.py
@@ -1,6 +1,30 @@
-import json
-import urllib2
+
+import requests
+import pandas as pd
+
+#%%
 
 url = "https://www.car2go.com/api/v2.1/vehicles?loc=duesseldorf&oauth_consumer_key=car2gowebsite&format=json"
 
-data = json.load(urllib2.urlopen(url))
+r = requests.get(url)
+
+#%%
+
+r.status_code
+r.headers
+#r.encoding
+#r.text
+
+r.status_code == requests.codes.ok
+
+#%%
+
+# jdta = r.json()
+# same: data = json.loads(r.text)
+
+#%%
+
+# car2go returns everything inside a placemarks [], so we normalise here
+data = pd.io.json.json_normalize(r.json(), 'placemarks')
+
+#%%
